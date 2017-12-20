d6c76fca569e2db54a3a3c150f9e482c96fff569
Heshan Perea <heshananupama@gmail.com>
2017-12-20 23:44:00 +0530

    empno

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('employeeNo')->unique();
+            $table->string('employeeno')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
