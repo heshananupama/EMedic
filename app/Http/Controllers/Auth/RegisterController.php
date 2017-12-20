b7a0823a9af6c10d2d21b320002b4aa5c9ac3aad
Heshan Perea <heshananupama@gmail.com>
2017-12-20 23:50:40 +0530

    empno

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'employeeNo' => $data['employeeNo'],
+            'employeeno' => $data['employeeNo'],
             'password' => bcrypt($data['password']),
         ]);
     }
